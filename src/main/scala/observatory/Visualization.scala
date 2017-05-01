8e61f0dc1bd73a1679907b452b4a7a7e716e708f
alexandrosmilaios <amilaios@icloud.com>
2017-05-01 23:48:49 +0100

    find color based on linear interpolation

--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -11,6 +11,7 @@ object Visualization {
 
   /**
     * Convert A location angle to radians
+    *
     * @param location Input location
     * @return a converted location
     */
@@ -27,7 +28,7 @@ object Visualization {
     val arcCosInput = math.sin(loc1.lat) * math.sin(loc2.lat) +
       math.cos(loc1.lat) * math.cos(loc2.lat) * math.cos(math.abs(loc1.lon - loc2.lon))
 
-    arcCosInput match  {
+    arcCosInput match {
       case _ if arcCosInput >= 1 => math.acos(1)
       case _ if arcCosInput <= -1 => math.acos(-1)
       case _ => math.acos(arcCosInput)
@@ -36,7 +37,7 @@ object Visualization {
 
   /**
     * @param distance the distance from the target location
-    * @param p indicates the power of the denominator
+    * @param p        indicates the power of the denominator
     * @return the weight for the Inverse distance weighting formula
     */
   def calculateWeight(distance: Double, p: Int = P): Double = {
@@ -45,14 +46,14 @@ object Visualization {
 
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
-    * @param location Location where to predict the temperature
+    * @param location     Location where to predict the temperature
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double = {
 
     val convertedLocation = convertDegreesToRadiant(location)
 
-    val distances = temperatures.par.map{ case (loc, temp) =>
+    val distances = temperatures.par.map { case (loc, temp) =>
       (calculateSphericalDistance(convertedLocation, convertDegreesToRadiant(loc)), temp)
     }
 
@@ -61,24 +62,68 @@ object Visualization {
     if (lessThanOneKM.isDefined) {
       lessThanOneKM.get._2
     } else {
-      val weights = distances.map{case (dist, temp) => (calculateWeight(dist), temp)}
-      val (numerator, denominator)= weights.foldLeft((0d,0d))((s, e) => (s._1 + e._1 * e._2, s._2 + e._1))
+      val weights = distances.map { case (dist, temp) => (calculateWeight(dist), temp) }
+      val (numerator, denominator) = weights.foldLeft((0d, 0d))((s, e) => (s._1 + e._1 * e._2, s._2 + e._1))
       numerator / denominator
     }
   }
 
   /**
     * @param points Pairs containing a value and its associated color
-    * @param value The value to interpolate
+    * @param value  The value to interpolate
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
   def interpolateColor(points: Iterable[(Double, Color)], value: Double): Color = {
-    ???
+
+    val max = points.maxBy(_._1)
+    val min = points.minBy(_._1)
+
+    def keep2ClosestPoints(seq: Seq[(Double, Color)], point: (Double, Color)): Seq[(Double, Color)] = {
+      if (seq.isEmpty || seq.size < 2) {
+        seq :+ point
+      } else {
+        val dif = math.abs(point._1 - value)
+        val max = seq.maxBy(x => math.abs(x._1 - value))
+
+        if (math.abs(max._1 - value) > dif) {
+          seq.filter(x => x != max) :+ point
+        } else {
+          seq
+        }
+      }
+    }
+
+    def unWrap2Points(twoPoints: Seq[(Double, Color)]): Color = {
+      twoPoints match {
+        case h :: Nil => h._2
+        case first :: second :: Nil =>
+          value match {
+            case _ if first._1 - value == 0 => first._2
+            case _ if second._1 - value == 0 => second._2
+            case _ =>
+              val xDif = second._1 - first._1
+              Color(
+                math.round(first._2.red + (value - first._1) * (second._2.red - first._2.red) / xDif).toInt,
+                math.round(first._2.green + (value - first._1) * (second._2.green - first._2.green) / xDif).toInt,
+                math.round(first._2.blue + (value - first._1) * (second._2.blue - first._2.blue) / xDif).toInt
+              )
+          }
+        case _ => throw new Error("Sequence should have only 2 elements!")
+      }
+    }
+
+    value match {
+      case _ if value > max._1 => max._2
+      case _ if value < min._1 => min._2
+      case _ =>
+        val twoPoints = points.foldLeft(Seq[(Double, Color)]()) (keep2ClosestPoints)
+        unWrap2Points(twoPoints)
+    }
   }
 
   /**
     * @param temperatures Known temperatures
-    * @param colors Color scale
+    * @param colors       Color scale
     * @return A 360×180 image where each pixel shows the predicted temperature at its location
     */
   def visualize(temperatures: Iterable[(Location, Double)], colors: Iterable[(Double, Color)]): Image = {
--- src/test/scala/observatory/VisualizationTest.scala
+++ src/test/scala/observatory/VisualizationTest.scala
@@ -6,13 +6,20 @@ import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.prop.Checkers
 
+import Visualization._
+
 @RunWith(classOf[JUnitRunner])
 class VisualizationTest extends FunSuite with Checkers {
+  val colors =
+    List((60.0, Color(255,255,255)), (32.0, Color(255, 0, 0)),
+      (12.0, Color(255, 255, 0)), (0.0, Color(0, 255, 255)),
+      (-15.0, Color(0, 0, 255)), (-27.0, Color(255, 0, 255)),
+      (-50.0, Color(33, 0, 107)), (-60.0, Color(0, 0, 0)))
 
 
   test("calculate distance") {
-    val loc = Visualization.convertDegreesToRadiant(Location(45.0, 45.0))
-    val actual = Visualization.calculateSphericalDistance(Location(0.0, 0.0), loc)
+    val loc = convertDegreesToRadiant(Location(45.0, 45.0))
+    val actual = calculateSphericalDistance(Location(0.0, 0.0), loc)
     val expected =  math.acos(0.5d)
     assert(math.abs(actual -expected) < 0.0001, "Should return the correct distance")
   }
@@ -20,7 +27,7 @@ class VisualizationTest extends FunSuite with Checkers {
   test("test predictTemperature function with less that 1KM distance") {
     val stationsData = Seq((Location(0.0, 0.0), 10.0), (Location(45.0, 0.0), 15.0))
     val location = Location(0.0, 0.0)
-    val actual = Visualization.predictTemperature(stationsData, location)
+    val actual = predictTemperature(stationsData, location)
     assert(actual === 10.0, "Should be the same temperature as the first station")
   }
 
@@ -30,12 +37,12 @@ class VisualizationTest extends FunSuite with Checkers {
     val distances = stationsData.map(x => (Visualization.calculateSphericalDistance(location, x._1), x._2))
     val weights = distances.map(x => (Visualization.calculateWeight(x._1), x._2))
     val expected = weights.map(x => x._1 * x._2).sum / weights.map(_._1).sum
-    val actual = Visualization.predictTemperature(stationsData, location)
+    val actual = predictTemperature(stationsData, location)
     assert(math.abs(actual - expected) < 0.0000001, "Should have the expected temperature")
   }
 
   test("Sample test") {
-    val result = Visualization.predictTemperature(List((Location(0.0,0.0),10.0)), location=Location(88.0,-174.0))
+    val result = predictTemperature(List((Location(0.0,0.0),10.0)), location=Location(88.0,-174.0))
     assert(result === 10.0)
   }
 
@@ -48,19 +55,43 @@ class VisualizationTest extends FunSuite with Checkers {
       (location1, temp1),
       (location2, temp2)
     )
-    val result = Visualization.predictTemperature(list, Location(0, 0))
+    val result = predictTemperature(list, Location(0, 0))
     assert(math.abs(temp1 - result) < math.abs(temp2 - result))
   }
 
   test("greatCircleDistance test (extreme case 1)") {
     val loc1 = Visualization.convertDegreesToRadiant(Location(-12.0, 85.0))
     val loc2 = Visualization.convertDegreesToRadiant(Location(12.0, -95.0))
-    assert(Visualization.calculateSphericalDistance(loc1,loc2 ) === math.Pi)
+    assert(calculateSphericalDistance(loc1,loc2 ) === math.Pi)
   }
 
   test("greatCircleDistance test (extreme case 2)") {
     val loc1 = Visualization.convertDegreesToRadiant(Location(90, -180))
     val loc2 = Visualization.convertDegreesToRadiant(Location(0.0, 0.0))
-    assert(math.abs(Visualization.calculateSphericalDistance(loc1,loc2 ) - 1.57079633D) < 0.00001)
+    assert(math.abs(calculateSphericalDistance(loc1,loc2 ) - 1.57079633D) < 0.00001)
+  }
+
+  test("Color interpolation 1") {
+    val scale = List((0.0, Color(0, 0, 255)))
+    assert(interpolateColor(scale, -0.5) == Color(0, 0, 255))
+    assert(interpolateColor(scale, 0.5) == Color(0, 0, 255))
+    assert(interpolateColor(scale, 0.0) == Color(0, 0, 255))
+  }
+
+  test("Color interpolation 2") {
+    assert(interpolateColor(colors, 12.0) == Color(255, 255, 0))
+  }
+
+  test("Color interpolation 3") {
+
+    assert(interpolateColor(colors, 62) == Color(255, 255, 255))
+  }
+
+  test("Color interpolation 4") {
+    assert(interpolateColor(colors, 6) == Color(128, 255, 128))
+  }
+
+  test("Color interpolation 5") {
+    assert(interpolateColor(colors, -7.5) == Color(0, 128, 255))
   }
 }
