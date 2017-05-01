669b2736eacd08ea13960de0da35973346d3b9be
alexandrosmilaios <amilaios@icloud.com>
2017-05-01 12:21:45 +0100

    Predict location implementation

--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -7,13 +7,64 @@ import com.sksamuel.scrimage.{Image, Pixel}
   */
 object Visualization {
 
+  val P: Int = 3
+
+  /**
+    * Convert A location angle to radians
+    * @param location Input location
+    * @return a converted location
+    */
+  def convertDegreesToRadiant(location: Location): Location = {
+    Location(location.lat.toRadians, location.lon.toRadians)
+  }
+
+  /**
+    * @param loc1 The coordinates of the first location
+    * @param loc2 The coordinates of the second location
+    * @return The Spherical distance between both input points
+    */
+  def calculateSphericalDistance(loc1: Location, loc2: Location): Double = {
+    val arcCosInput = math.sin(loc1.lat) * math.sin(loc2.lat) +
+      math.cos(loc1.lat) * math.cos(loc2.lat) * math.cos(math.abs(loc1.lon - loc2.lon))
+
+    arcCosInput match  {
+      case _ if arcCosInput >= 1 => math.acos(1)
+      case _ if arcCosInput <= -1 => math.acos(-1)
+      case _ => math.acos(arcCosInput)
+    }
+  }
+
+  /**
+    * @param distance the distance from the target location
+    * @param p indicates the power of the denominator
+    * @return the weight for the Inverse distance weighting formula
+    */
+  def calculateWeight(distance: Double, p: Int = P): Double = {
+    1 / math.pow(distance, p)
+  }
+
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
     * @param location Location where to predict the temperature
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double = {
-    ???
+
+    val convertedLocation = convertDegreesToRadiant(location)
+
+    val distances = temperatures.par.map{ case (loc, temp) =>
+      (calculateSphericalDistance(convertedLocation, convertDegreesToRadiant(loc)), temp)
+    }
+
+    val lessThanOneKM = distances.find(_._1 <= 0.001)
+
+    if (lessThanOneKM.isDefined) {
+      lessThanOneKM.get._2
+    } else {
+      val weights = distances.map{case (dist, temp) => (calculateWeight(dist), temp)}
+      val (numerator, denominator)= weights.foldLeft((0d,0d))((s, e) => (s._1 + e._1 * e._2, s._2 + e._1))
+      numerator / denominator
+    }
   }
 
   /**
--- src/test/scala/observatory/VisualizationTest.scala
+++ src/test/scala/observatory/VisualizationTest.scala
@@ -10,4 +10,57 @@ import org.scalatest.prop.Checkers
 class VisualizationTest extends FunSuite with Checkers {
 
 
+  test("calculate distance") {
+    val loc = Visualization.convertDegreesToRadiant(Location(45.0, 45.0))
+    val actual = Visualization.calculateSphericalDistance(Location(0.0, 0.0), loc)
+    val expected =  math.acos(0.5d)
+    assert(math.abs(actual -expected) < 0.0001, "Should return the correct distance")
+  }
+
+  test("test predictTemperature function with less that 1KM distance") {
+    val stationsData = Seq((Location(0.0, 0.0), 10.0), (Location(45.0, 0.0), 15.0))
+    val location = Location(0.0, 0.0)
+    val actual = Visualization.predictTemperature(stationsData, location)
+    assert(actual === 10.0, "Should be the same temperature as the first station")
+  }
+
+  test("test predictTemperature function") {
+    val stationsData = Seq((Location(0.0, 0.0), 10.0), (Location(90.0, 0.0), 15.0))
+    val location = Location(45.0, 0.0)
+    val distances = stationsData.map(x => (Visualization.calculateSphericalDistance(location, x._1), x._2))
+    val weights = distances.map(x => (Visualization.calculateWeight(x._1), x._2))
+    val expected = weights.map(x => x._1 * x._2).sum / weights.map(_._1).sum
+    val actual = Visualization.predictTemperature(stationsData, location)
+    assert(math.abs(actual - expected) < 0.0000001, "Should have the expected temperature")
+  }
+
+  test("Sample test") {
+    val result = Visualization.predictTemperature(List((Location(0.0,0.0),10.0)), location=Location(88.0,-174.0))
+    assert(result === 10.0)
+  }
+
+  test("predictTemperature: some point closer") {
+    val location1 = Location(1,1)
+    val temp1 = 10d
+    val location2 = Location(-10,-10)
+    val temp2 = 50d
+    val list = List(
+      (location1, temp1),
+      (location2, temp2)
+    )
+    val result = Visualization.predictTemperature(list, Location(0, 0))
+    assert(math.abs(temp1 - result) < math.abs(temp2 - result))
+  }
+
+  test("greatCircleDistance test (extreme case 1)") {
+    val loc1 = Visualization.convertDegreesToRadiant(Location(-12.0, 85.0))
+    val loc2 = Visualization.convertDegreesToRadiant(Location(12.0, -95.0))
+    assert(Visualization.calculateSphericalDistance(loc1,loc2 ) === math.Pi)
+  }
+
+  test("greatCircleDistance test (extreme case 2)") {
+    val loc1 = Visualization.convertDegreesToRadiant(Location(90, -180))
+    val loc2 = Visualization.convertDegreesToRadiant(Location(0.0, 0.0))
+    assert(math.abs(Visualization.calculateSphericalDistance(loc1,loc2 ) - 1.57079633D) < 0.00001)
+  }
 }
