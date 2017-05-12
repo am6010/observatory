fd9fbb38ff84aff924e7c3e2b4d2f8980c3cfd2f
alexandrosmilaios <amilaios@icloud.com>
2017-05-12 09:05:41 +0100

    manipulation : implementation of makeGrid

--- src/main/scala/observatory/Manipulation.scala
+++ src/main/scala/observatory/Manipulation.scala
@@ -2,6 +2,7 @@ package observatory
 
 import Visualization._
 
+import scala.collection.immutable.IndexedSeq
 import scala.collection.parallel.immutable.ParMap
 
 /**
@@ -9,18 +10,19 @@ import scala.collection.parallel.immutable.ParMap
   */
 object Manipulation {
 
+  val cor: IndexedSeq[Location] = for {
+    lat <- -89 to 90 by 1
+    lon <- -180 to 179 by 1
+  } yield Location(lat, lon)
+
   /**
     * @param temperatures Known temperatures
     * @return A function that, given a latitude in [-89, 90] and a longitude in [-180, 179],
     *         returns the predicted temperature at this location
     */
   def makeGrid(temperatures: Iterable[(Location, Double)]): (Int, Int) => Double = {
-   val cor = for {
-      lat <- -89 to 90 by 1
-      lon <- -180 to 179 by 1
-    } yield Location(lat, lon)
 
-    val grid: ParMap[(Int,Int), Double]= cor.par.foldLeft(ParMap.empty[(Int, Int), Double]) { (map, loc) =>
+    val grid: ParMap[(Int, Int), Double] = cor.par.foldLeft(ParMap.empty[(Int, Int), Double]) { (map, loc) =>
       map + ((loc.lat.toInt, loc.lon.toInt) -> predictTemperature(temperatures, loc))
     }
 
@@ -33,12 +35,18 @@ object Manipulation {
     * @return A function that, given a latitude and a longitude, returns the average temperature at this location
     */
   def average(temperaturess: Iterable[Iterable[(Location, Double)]]): (Int, Int) => Double = {
-    ???
+    val years = temperaturess.size
+    val map = temperaturess
+      .map(temperatures => makeGrid(temperatures))
+      .flatMap(grid => cor.map(x => ((x.lat.toInt, x.lon.toInt), grid(x.lat.toInt, x.lon.toInt))))
+      .groupBy(_._1)
+      .mapValues(x => x.map(_._2).sum / years)
+    (lat, lon) => map((lat, lon))
   }
 
   /**
     * @param temperatures Known temperatures
-    * @param normals A grid containing the “normal” temperatures
+    * @param normals      A grid containing the “normal” temperatures
     * @return A sequence of grids containing the deviations compared to the normal temperatures
     */
   def deviation(temperatures: Iterable[(Location, Double)], normals: (Int, Int) => Double): (Int, Int) => Double = {
